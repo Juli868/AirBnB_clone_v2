659fb15d01b57d97c377bb11286b525e16a2bd25
juli868 <bugijul@gmail.com>
2023-12-07 03:46:45 +0200

    Prep servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,5 +6,5 @@ sudo mkdir /data/web_static/releases
 sudo mkdir /data/web_static/shared/
 sudo mkdir /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
-ln -s /data/web_static/current /data/web_static/releases/test/
-chown  -R ubuntu :ubuntu /data
+sudo ln -s /data/web_static/current /data/web_static/releases/test/
+sudo chown  -R ubuntu :ubuntu /data
