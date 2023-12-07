d034ad1f9bc28dd8eaed55e8c339e9d4e557b6ea
juli868 <bugijul@gmail.com>
2023-12-07 03:30:22 +0200

    preparing web  servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 #preparing web servers
-mkdir /data/
-mkdir /data/web_static
-mkdir /data/web_static/releases
-mkdir /data/web_static/shared/
-mkdir /data/web_static/releases/test/
-touch /data/web_static/releases/test/index.html
+sudo mkdir /data/
+sudo mkdir /data/web_static
+sudo mkdir /data/web_static/releases
+sudo mkdir /data/web_static/shared/
+sudo mkdir /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
 ln -s /data/web_static/current /data/web_static/releases/test/
-chown  -r ubuntu ubuntu /data
+chown  -R ubuntu :ubuntu /data
