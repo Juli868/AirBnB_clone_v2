faeaa068cbf0557338829c8004ba9a0ca9731c04
juli868 <bugijul@gmail.com>
2023-12-11 02:24:22 +0200

    Second task

---
+++ 1-pack_web_static.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Create a zip file."""
+import tarfile
+import os
+from datetime import datetime
+
+
+def do_pack():
+    """Create a .tgz zip file."""
+    now = datetime.now()
+    file_name = f'web_static_{now.year}{now.month}{now.date}
+    {now.hour}{now.minute}{now.seconds}.tgz'
+    with tarfile.open(file_name, 'w:gz') as new:
+        files = glob.glob(os.path.join('web_static', '*'))
+        for file_n in files:
+            new.add(file_n, arcname=os.path.basement(file_n))
