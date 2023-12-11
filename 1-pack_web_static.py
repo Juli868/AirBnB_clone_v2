a65de9fb3a6258cc8982399fc83cfeb9a589ca97
juli868 <bugijul@gmail.com>
2023-12-11 02:37:04 +0200

    escaping EOL

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,8 +8,8 @@ from datetime import datetime
 def do_pack():
     """Create a .tgz zip file."""
     now = datetime.now()
-    file_name = f'web_static_{now.year}{now.month}{now.date}
-    {now.hour}{now.minute}{now.seconds}.tgz'
+    file_name = f'web_static_{now.year}{now.month}{now.date}'\
+    '{now.hour}{now.minute}{now.seconds}.tgz'
     with tarfile.open(file_name, 'w:gz') as new:
         files = glob.glob(os.path.join('web_static', '*'))
         for file_n in files:
