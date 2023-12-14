f8413c75b5f5777e19fbdd2ccd95ec8ba7123da8
juli868 <bugijul@gmail.com>
2023-12-15 00:17:36 +0200

    first two tasks

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 #preparing web servers
-sudo mkdir /data/
-sudo mkdir /data/web_static
-sudo mkdir /data/web_static/releases
-sudo mkdir /data/web_static/shared/
-sudo mkdir /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo echo "hello world!" > /data/web_static/releases/test/index.html
 sudo ln -s /data/web_static/current /data/web_static/releases/test/
 sudo chown  -R ubuntu:ubuntu /data
 if grep -q "server{/n location /hbnb_static{/n alias /data/web_static/current/;/n }/n}" "/etc/nginx/sites-available/default";then
--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,8 +8,9 @@ from datetime import datetime
 def do_pack():
     """Create a .tgz zip file."""
     now = datetime.now()
-    file_name = f'web_static_{now.year}{now.month}{now.date}'\
-    '{now.hour}{now.minute}{now.seconds}.tgz'
+    file_name = f'web_static_{datetime.now.year}{datetime.now.month}'
+    f'{datetime.now.date}{datetime.now.hour}{datetime.now.minute}'
+    f'{datetime.now.seconds}.tgz'
     with tarfile.open(file_name, 'w:gz') as new:
         files = glob.glob(os.path.join('web_static', '*'))
         for file_n in files:
