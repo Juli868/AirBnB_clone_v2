84b46da5a4fbc20d806351f8b4ef412e69293d7a
juli868 <bugijul@gmail.com>
2023-12-15 00:55:53 +0200

    Feaw tweeks

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,15 +3,15 @@
 import tarfile
 import os
 from datetime import datetime
-
+import glob
 
 def do_pack():
     """Create a .tgz zip file."""
     now = datetime.now()
-    file_name = f'web_static_{datetime.now.year}{datetime.now.month}'
-    f'{datetime.now.date}{datetime.now.hour}{datetime.now.minute}'
-    f'{datetime.now.seconds}.tgz'
+    file_name = f'web_static_{now.year}{now.month}'
+    f'{now.date}{now.hour}{now.minute}'
+    f'{now.seconds}.tgz'
     with tarfile.open(file_name, 'w:gz') as new:
         files = glob.glob(os.path.join('web_static', '*'))
         for file_n in files:
-            new.add(file_n, arcname=os.path.basement(file_n))
+            new.add(file_n, arcname=os.path.basename(file_n))
