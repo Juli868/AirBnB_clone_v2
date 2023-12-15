ecea48f8a75fef4f372de866c74568155d1d5b1a
juli868 <bugijul@gmail.com>
2023-12-15 04:53:41 +0200

    Second task

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,7 +9,7 @@ import glob
 def do_pack():
     """Create a .tgz zip file."""
     now = datetime.utcnow()
-    file_name = f'web_static_{now.year}{now.month}{now.date}'\
+    file_name = f'web_static_{now.year}{now.month}{now.date}'
     f'{now.hour}{now.minute}{now.seconds}.tgz'
     folder = 'versions'
     with tarfile.open(os.path.join(folder, file_name), 'w:gz') as new:
