3734229a51557912a3e514123b31ffcc44589f79
juli868 <bugijul@gmail.com>
2023-12-15 05:01:08 +0200

    Adding a return value

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,10 +9,13 @@ import glob
 def do_pack():
     """Create a .tgz zip file."""
     now = datetime.utcnow()
-    file_name = f'web_static_{now.year}{now.month}{now.date}'
+    file_name = f'versions/web_static_{now.year}{now.month}{now.date}'
     f'{now.hour}{now.minute}{now.seconds}.tgz'
-    folder = 'versions'
-    with tarfile.open(os.path.join(folder, file_name), 'w:gz') as new:
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    with tarfile.open(file_name, 'w:gz') as new:
         files = glob.glob(os.path.join('web_static', '*'))
         for file_n in files:
             new.add(file_n, arcname=os.path.basename(file_n))
+    return filename
