94cfd7033504347f6f042366840d78e5c85b585d
juli868 <bugijul@gmail.com>
2023-12-23 15:55:05 +0200

    Flask wam-up

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+from flask import Flask, render_template, request
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return("Hello world!")
---
+++ web_flask/README.md
@@ -0,0 +1,1 @@
+Working on ccreating the web with flask flamework to generate the HTML and css but through writing only Python codes. awesome right?!
