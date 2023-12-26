060c1f52100d559fdb5b752faf3423eef2016445
juli868 <bugijul@gmail.com>
2023-12-26 09:59:35 +0200

    second task

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,1 +1,7 @@
+#!/usr/bin/python3
+import os
+from fabric.api import local
+
 def do_deploy(archive_path):
+    if os.path.isdir(archive_path) is False:
+        return False
---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#!/us/bin/python3
+"""Routing definition."""
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def index():
+    """Route for home."""
+    return ("Hello HBNB!")
+
+
+@app.route("/hbnb")
+def hbnb():
+    """Hbnb route."""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
