32b6af9a702fc68d5e6579aec19eb8dfd09dffec
juli868 <bugijul@gmail.com>
2023-12-30 15:31:33 +0200

    Fifth task

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Add a new route."""
+from flask import Flask
+import json
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    """Define home route."""
+    return "Hello HBNB"
+
+
+@app.route("/hbnb")
+def HBNB():
+    """Define hbnb route."""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_text(text):
+    """C followed by a text."""
+    return f"C {text.replace('_',' ')}"
+
+
+@app.route("/python/")
+@app.route("/python/<text>")
+def python_text(text='is cool'):
+    """Define Python route."""
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route("/number/<n>")
+def number(n):
+    """Displaying only a number."""
+    try:
+        number = int(n)
+        return f"{number} is a number"
+    except TypeError as e:
+        return None
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
