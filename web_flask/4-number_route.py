b0757db4fe7047ab6bde219d188f3b9906bb2e45
juli868 <bugijul@gmail.com>
2023-12-30 19:51:10 +0200

    sixth task

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -37,8 +37,13 @@ def number(n):
     try:
         number = int(n)
         return f"{number} is a number"
-    except TypeError as e:
-        return None
+    except Exception as e:
+        return '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">\n\
+                <title>404 Not Found</title>\n\
+                <h1>Not Found</h1>\n\
+                <p>The requested URL was not found on the server.  \
+                If you entered the URL manually please check your \
+                spelling and try again.</p>'
 
 
 if __name__ == '__main__':
---
+++ web_flask/5-number_template.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""Add a new route."""
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    """Define home route."""
+    return "Hello HBNB"
+
+
+@app.route("/hbnb")
+def HBNB():
+    """Define hbnb route."""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_text(text):
+    """C followed by a text."""
+    return f"C {text.replace('_',' ')}"
+
+
+@app.route("/python/")
+@app.route("/python/<text>")
+def python_text(text='is cool'):
+    """Define Python route."""
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route("/number/<n>")
+def number(n):
+    """Display only a number."""
+    try:
+        number = int(n)
+        return f"{number} is a number"
+    except Exception as e:
+        return '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">\n\
+                <title>404 Not Found</title>\n\
+                <h1>Not Found</h1>\n\
+                <p>The requested URL was not found on the server.  \
+                If you entered the URL manually please check your \
+                spelling and try again.</p>'
+
+
+@app.route('/number_template/<n>')
+def number_template(n):
+    """Display only numbers if int."""
+    try:
+        number = int(n)
+        return render_template("5-number.html", number=number)
+    except Exception as e:
+        return '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">\n\
+                <title>404 Not Found</title>\n\
+                <h1>Not Found</h1>\n\
+                <p>The requested URL was not found on the server.  \
+                If you entered the URL manually please check your \
+                spelling and try again.</p>'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
